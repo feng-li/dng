2046865018f5da4ced249e30c218573ea4ec39c2
DESKTOP-V9HQUA6\Zeng <zengjiayue@126.com>
2018-02-19 01:44:41 +0800

    Add split_normal.cpp

---
+++ src/split_normal.cpp
@@ -0,0 +1,74 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+
+// [[Rcpp::export]]
+List AsymNormPDF_cpp(NumericVector y,NumericVector mu, NumericVector sigma, NumericVector lmd, bool cum, bool sumStat)
+{
+  int i,j,n,len,a;
+  len = y.size();
+  NumericVector density(len),densMean(len),densStd(len);
+  NumericVector I0(len),I(len), sign(len);
+  
+  if(cum) //cum=1 PDF
+  {
+    for(int a=0;a<len;a++)
+    {
+      I0[a]=(y[a]<=mu[a]);
+      I[a]= 1-I0[a];
+      sign[a]=1*I0[a]+lmd[a]*lmd[a]*I[a];
+      density[a] = sqrt(2/3.1415926)*
+        exp(-pow((y[a]-mu[a]),2)/(2*sigma[a]*sigma[a]*sign[a]))/
+          ((1+lmd[a])*sigma[a]);
+    }
+    
+  }
+  
+  else if(!cum) //cum=0 CDF
+  {
+    for(int a=0;a<len;a++)
+    {
+      I0[a]=(y[a]<=mu[a]);
+      I[a]= 1-I0[a];
+      sign[a]=1*I0[a]+lmd[a]*lmd[a]*I[a];
+      if(y[a]<=mu[a])
+      {
+        density[a] =2/(1+lmd[a])*R::pnorm5(y[a],mu[a],sigma[a],1,0);
+        
+      }
+      else if(y[a]>mu[a])
+      {
+        density[a] = 1/(1+lmd[a]) + 
+          2*lmd[a]/(1+lmd[a])*(R::pnorm5(y[a],mu[a],sigma[a],1,0)-1/2); 
+      }
+    }
+  }
+  
+  
+  if(sumStat)
+  {
+    NumericVector PostC(len),Var(len);
+    
+    for(int a=0;a<len;a++)
+    {
+      PostC[a] = sqrt(2/3.1415926)*sigma[a]*(lmd[a]-1);
+      densMean[a] = mu[a]+PostC[a];
+      Var[a] = pow(pow(((3.1415926-2)/3.1415926*pow((lmd[a]-1),2)+lmd[a]),sigma[a]),2);
+      densStd[a] = sqrt(Var[a]);
+    }
+    
+  }
+  else if(!sumStat)
+  {
+    densMean = NA;
+    densStd = NA;
+    
+  }
+  
+  List out= List::create(_["density"] = density,
+                         _["densMean"] = densMean,
+                         _["densStd"] = densStd ) ;
+  
+  return out;
+  
+}
