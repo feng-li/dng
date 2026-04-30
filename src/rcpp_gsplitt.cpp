b47be71511d07f123d3d6c8d40c31b4b0795096d
Feng Li <m@feng.li>
2026-04-30 09:22:16 +0800

    Implement gradient for splitt

--- src/rcpp_gsplitt.cpp
+++ src/rcpp_gsplitt.cpp
@@ -1,263 +1,266 @@
 #include <Rcpp.h>
+
+#include <algorithm>
+#include <cctype>
+#include <cmath>
+#include <limits>
+#include <string>
+#include <vector>
+
 using namespace Rcpp;
-//' @describeIn splitt ghypergeo for the split-t distribution.
-//' @export
-// [[Rcpp::export]]
-NumericMatrix ghypergeo(NumericMatrix a, NumericMatrix b, NumericVector z,int k);
-List gsplitt(NumericVector y, List par, std::string parCaller, GenericVector denscaller)
+
+namespace {
+
+const double kTol = 1e-12;
+const int kMaxTerms = 100000;
+
+std::string lower_string(std::string value)
 {
-  int i,j,n1,n3;
+  std::transform(value.begin(), value.end(), value.begin(),
+                 [](unsigned char c) { return std::tolower(c); });
+  return value;
+}
 
-  n1=y.size();
-  // n2=type.size();
-  n3=parCaller.size();
-  // n4=denscaller.size();
+bool starts_with_flag(const GenericVector& values, const char flag)
+{
+  for (R_xlen_t i = 0; i < values.size(); ++i) {
+    if (Rf_isNull(values[i])) {
+      continue;
+    }
 
-  NumericVector mu(n1),df(n1),phi(n1),lmd(n1),logMargiDens(n1);
-  NumericVector outu(n1),outd(n1);
-
-  // for(i=0;i<n2;i++){
-  //   if(type[i]>=65 && type[i]<=90)
-  //     type[i] = 32+type[i]; }
-
-  for(i=0;i<n3;i++){
-    if(parCaller[i]>=65 && parCaller[i]<=90)
-      parCaller[i] = 32+parCaller[i];
+    std::string value = lower_string(as<std::string>(values[i]));
+    if (!value.empty() && value[0] == flag) {
+      return true;
+    }
   }
 
+  return false;
+}
 
+double hypergeo_pfq(const std::vector<double>& a,
+                    const std::vector<double>& b,
+                    const double z,
+                    const int max_terms = kMaxTerms,
+                    const double tol = kTol)
+{
+  if (!std::isfinite(z)) {
+    return R_NaN;
+  }
 
-  mu  = par["mu"];
-  df  = par["df"];
-  phi = par["phi"];
-  lmd = par["lmd"];
-  // PDF
-  // NumericVector MargiDens;
-  // MargiDens[i] = R::dsplitt(y, mu,  df,  phi,  lmd, FALSE)
+  double term = 1.0;
+  double sum = 1.0;
 
-  if(parCaller == "mu")
-    {
-      if(std::strncmp(denscaller[0],"u",1)==0)
-        {
-          NumericVector I0(n1),I(n1);
-          NumericVector sign(n1);
-          NumericVector beta0(n1);
+  for (int n = 1; n <= max_terms; ++n) {
+    double ratio = z / static_cast<double>(n);
 
-          for(int a=0;a<n1;a++)
-            {
-              I0[a]=(y[a]<=mu[a]);
-              I[a]= 1-I0[a];
-              sign[a]=1*I0[a]+lmd[a]*I[a];
-              beta0[a]=R::beta(df[a]/2, 1/2);
-            }
-
-          for(j=0;j<n1;j++)
-            {
-              outu[j] = -2*sign[j]*sqrt(1/(pow((y[j]-mu[j]),2)+pow(sign[j],2)*df[j]*pow(phi[j],2)))*
-                pow((pow(sign[j],2)*df[j]*pow(phi[j],2)/
-                     (pow((y[j]-mu[j]),2)+pow(sign[j],2)*df[j]*pow(phi[j],2))),(df[j]/2))/
-                ((1+lmd[j])*beta0[j]);
-
-            }
-
-        }
-      if(std::strncmp(denscaller[1],"d",1)==0)
-        {
-          // NOTE: This is the gradient with respect to the log density
-          NumericVector I0(n1),I(n1), Sign(n1),beta0(n1);
-
-          for(int a=0;a<n1;a++){
-            I0[a]=(y[a]<=mu[a]);
-            I[a]= 1-I0[a];
-            Sign[a]=1*I0[a]+lmd[a]*lmd[a]*I[a];}
-          for(j=0;j<n1;j++)
-            {
-
-              outd[j] = -(1+df[j])*(mu[j]-y[j])/(pow((mu[j]-y[j]),2)+pow(phi[j],2)*df[j]*Sign[j]);
-
-            }
-        }
+    for (std::size_t i = 0; i < a.size(); ++i) {
+      ratio *= a[i] + n - 1.0;
     }
 
-
-  else if(parCaller == "df")
-    {
-      if(std::strncmp(denscaller[i],"u",1)==0)
-        {
-          double ibeta0;
-          NumericVector I0(n1),I(n1);
-          NumericVector sign(n1),sign2(n1);
-          NumericVector Z(n1),beta0(n1),ghy0(n1);
-          NumericVector digamma0(n1),digamma1(n1); //NumericVector digamma0,digamma1;
-          NumericMatrix A(n1,3), B(n1,2),ghy(n1,1);
-
-          for(int a=0;a<n1;a++){
-            I0[a]=(y[a]<=mu[a]);
-            I[a]= 1-I0[a];
-            sign[a]=I0[a]*(-1) + I[a]*1;
-            sign2[a]=I0[a]*(-1) + I[a]*1;
-            beta0[a]=R::beta(df[a]/2, 1/2);
-            digamma0[a]=R::digamma(df[a]/2);
-            digamma1[a]=R::digamma((df[a]+1)/2);
-
-            A(a,0)=0.5;
-            A(a,1)=df[a]/2;
-            A(a,2)=df[a]/2;
-            B(a,0)=1+df[a/2];
-            B(a,1)=1+df[a/2];
-
-            Z[a]=(df[a]*pow(phi[a],2)*pow(sign[a],2))/(pow((y[a]-mu[a]),2)+pow(sign[a],2)*df[a]*pow(phi[a],2));
-          }
-          ghy = ghypergeo(A, B, Z, 10);
-
-          for(j=0;j<n1;j++)
-            {
-              //ibeta0=ibeta(Z[j], df[j]*0.5, 0.5, FALSE, TRUE);
-              ibeta0 = exp(::Rf_pbeta(Z[j],df[j]*0.5, 0.5, TRUE, TRUE));
-              ghy0[j] = ghy(j,1);
-              outu[j] = (sign[j]/(2*(1+lmd[j])*pow(df[j],2)*beta0[j]))*
-                (sign2[j]*4*pow(Z[j],(df[j]/2))*ghy0[j]+(df[j]*(
-                                                                -2*(y[j]-mu[j])*sqrt(1/(pow((y[j]-mu[j]),2)+pow(sign[j],2)*df[j]*pow(phi[j],2)))*pow(Z[j],(df[j]/2))-
-                                                                sign2[j]*df[j]*ibeta0*
-                                                                (log(Z[j])-digamma0[j]+digamma1[j]))));
-
-            }
-        }
-
-      if(std::strncmp(denscaller[i],"d",1)==0)
-        {
-          NumericVector I0(n1),I(n1);
-          NumericVector Sign(n1);
-          NumericVector C1(n1), C0(n1), C3(n1);
-          NumericVector DigammaM(n1);
-          NumericMatrix A(n1,3), B(n1,2),ghy(n1,1);
-
-          for(int a=0;a<n1;a++){
-            I0[a]=(y[a]<=mu[a]);
-            I[a]= 1-I0[a];
-            Sign[a]=I0[a]*(-1) + I[a]*pow(lmd[a],2);
-
-            C1[a]  =  pow((mu[a]-y[a]),2)+pow(phi[a],2)*df[a]*Sign[a];
-            C0[a]  =  pow((mu[a]-y[a]),2)-pow(phi[a],2)*Sign[a];
-            C3[a]  =  log(df[a]/(df[a]+pow((mu[a]-y[a]),2)/(pow(phi[a],2)*Sign[a])));
-            DigammaM[a] = R::digamma(df[a]/2) - R::digamma((1+df[a])/2);
-
-          }
-          for(j=0;j<n1;j++)
-            {outd[j]=(C0[j]/C1[j] + C3[j]-DigammaM[j])/2;}
-        }
+    for (std::size_t i = 0; i < b.size(); ++i) {
+      const double denominator = b[i] + n - 1.0;
+      if (denominator == 0.0) {
+        return R_NaN;
+      }
+      ratio /= denominator;
     }
 
+    term *= ratio;
+    sum += term;
 
-  else if(parCaller == "phi")
-    {
-      if(std::strncmp(denscaller[i],"u",1)==0)
-        {
-          // NOTE: This is the gradient with respect to the CDF function(not the log form) .
-          NumericVector I0(n1),I(n1);
-          NumericVector sign(n1);
-          NumericVector beta0(n1);
-
-          for(int a=0;a<n1;a++){
-            I0[a]=(y[a]<=mu[a]);
-            I[a]= 1-I0[a];
-            sign[a]=1*I0[a]+lmd[a]*I[a];
-            beta0[a]=R::beta(df[a]/2, 1/2);}
-
-          for(j=0;j<n1;j++)
-            {outu[j]=-2*sign[j]*(y[j]-mu[j])*sqrt(1/pow((y[j]-mu[j]),2)+pow(sign[j],2)*df[j]*pow(phi[j],2))*
-                pow(pow(sign[j],2)*df[j]*pow(phi[j],2)/(pow((y[j]-mu[j]),2)+pow(sign[j],2)*df[j]*pow(phi[j],2)),(df[j]/2))/
-                ((1+lmd[j])*phi[j]*beta0[j]);}
-
-
-        }
-
-      if(std::strncmp(denscaller[i],"d",1)==0)
-        {
-          // NOTE: This is the gradient with respect to the log density
-          NumericVector I0(n1),I(n1);
-          NumericVector Sign(n1);
-          NumericVector beta0(n1);
-          NumericVector C1(n1),C0(n1);
-
-          for(int a=0;a<n1;a++){
-            I0[a]=(y[a]<=mu[a]);
-            I[a]= 1-I0[a];
-            Sign[a]=1*I0[a]+lmd[a]*lmd[a]*I[a];
-            C1[a]= pow((mu[a]-y[a]),2)+pow(phi[a],2)*df[a]*Sign[a];
-            C0[a]= pow((mu[a]-y[a]),2)-pow(phi[a],2)*Sign[a];}
-
-          for(j=0;j<n1;j++)
-            {outd[j]=df[j]*C0[j]/phi[j]/C1[j];}
-        }
-
+    if (!std::isfinite(sum) || !std::isfinite(term)) {
+      return R_NaN;
     }
 
+    if (std::fabs(term) <= tol * std::max(1.0, std::fabs(sum))) {
+      break;
+    }
+  }
 
-  else if(parCaller == "lmd")
-    {
-      if(std::strncmp(denscaller[i],"u",1)==0)
-        {
-          NumericVector I0(n1),I(n1);
-          NumericVector C1(n1), A(n1), B(n1);
-          NumericVector Sign(n1);
-          double ibeta0;
+  return sum;
+}
 
-          for(int a=0;a<n1;a++)
-            {
-              I0[a] =(y[a]<=mu[a]);
-              I[a] = 1-I0[a];
-              outu = mu;
-              for(int i=0;i<n1;i++)
-                {
-                  if(I0[i]==TRUE)
-                    {
-                      A[i] = df[i]*pow(phi[i],2)/(pow((y[i]-mu[i]),2)+df[i]*pow(phi[i],2));
-                      //ibeta0  = ibeta(A[i], df[i]*0.5, 0.5, FALSE, TRUE);
-                      ibeta0 = exp(::Rf_pbeta(A[i], df[i]*0.5, 0.5, TRUE, TRUE));
-                      outu[i] = -ibeta0/pow((1+lmd[i]),2);
+double beta_density(const double x, const double a, const double b)
+{
+  if (x <= 0.0 || x >= 1.0) {
+    return 0.0;
+  }
 
-                    }
+  return std::exp((a - 1.0) * std::log(x) +
+                  (b - 1.0) * std::log1p(-x) -
+                  ::Rf_lbeta(a, b));
+}
 
-                  if(I0[i]==FALSE)
-                    {
-                      B[i] = (pow((y[i]-mu[i]),2)+df[i]*pow(phi[i],2)*pow(lmd[i],2));
-                      A[i] = df[i]*pow(phi[i],2)*pow(lmd[i],2)/B[i];
-                      //ibeta0  = ibeta(A[i], df[i]*0.5, 0.5, FALSE, FALSE);
-                      ibeta0 = exp(::Rf_pbeta(A[i], df[i]*0.5, 0.5, TRUE, TRUE)+::Rf_lbeta(df[i]*0.5, 0.5));
+double regularized_beta_da(const double x, const double a, const double b)
+{
+  if (x <= 0.0 || x >= 1.0) {
+    return 0.0;
+  }
 
-                      outu[i] = -(2*(1+lmd[i])*(y[i]-mu[i])*sqrt(1/B[i])*pow(A[i],(df[i]/2))+
-                                  ibeta0/(pow((1+lmd[i]),2)*R::beta(df[i]/2,1/2)));
+  const double ix = ::Rf_pbeta(x, a, b, TRUE, FALSE);
+  const double h = hypergeo_pfq({a, a, 1.0 - b}, {a + 1.0, a + 1.0}, x);
+  const double tail = std::exp(a * std::log(x) -
+                               2.0 * std::log(a) -
+                               ::Rf_lbeta(a, b)) * h;
 
-                    }
-                }
-            }
-        }
-      if(std::strncmp(denscaller[i],"d",1)==0)
-        {
-          NumericVector I0(n1),I(n1);
-          NumericVector C1(n1);
-          NumericVector Sign(n1);
+  return ix * (std::log(x) - R::digamma(a) + R::digamma(a + b)) - tail;
+}
 
-          for(int a=0;a<n1;a++){
-            I0[a]=(y[a]<=mu[a]);
-            I[a]= 1-I0[a];
+double regularized_beta_dnu(const double x, const double nu)
+{
+  if (x <= 0.0 || x >= 1.0) {
+    return 0.0;
+  }
 
-            C1[a]=-((1+df[a]+df[a]*lmd[a])*pow((mu[a]-y[a]),2)-pow(lmd[a],3)*pow(phi[a],2)*df[a])/
-              lmd[a]/(pow((mu[a]-y[a]),2)+pow(lmd[a],2)*pow(phi[a],2)*df[a]);
-            Sign[a]= 1*I0[a] + C1[a]*I[a];
-          }
-          for(j=0;j<n1;j++)
-            {outd[j]=-1/(1+lmd[j])*Sign[j];}
+  const double a = 0.5 * nu;
+  const double b = 0.5;
+  const double d_da = regularized_beta_da(x, a, b);
+  const double d_dx = beta_density(x, a, b);
+  const double d_x_d_nu = x * (1.0 - x) / nu;
 
-        }
+  return 0.5 * d_da + d_dx * d_x_d_nu;
+}
+
+bool invalid_parameters(const double nu, const double phi, const double lambda)
+{
+  return !std::isfinite(nu) || !std::isfinite(phi) || !std::isfinite(lambda) ||
+    nu <= 0.0 || phi <= 0.0 || lambda <= 0.0;
+}
+
+} // namespace
+
+//' @describeIn splitt Generalized hypergeometric function for split-t gradient calculations.
+//' @param a matrix of upper hypergeometric parameters.
+//' @param b matrix of lower hypergeometric parameters.
+//' @param z vector of hypergeometric function arguments.
+//' @param k maximum number of hypergeometric series terms.
+//' @export
+// [[Rcpp::export]]
+NumericMatrix ghypergeo(NumericMatrix a, NumericMatrix b, NumericVector z, int k)
+{
+  const int n = std::max(static_cast<int>(z.size()),
+                         std::max(a.nrow(), b.nrow()));
+  const int max_terms = k > 0 ? k : kMaxTerms;
+  NumericMatrix out(n, 1);
+
+  for (int i = 0; i < n; ++i) {
+    std::vector<double> upper(a.ncol());
+    std::vector<double> lower(b.ncol());
+    const int a_row = a.nrow() == 1 ? 0 : i % a.nrow();
+    const int b_row = b.nrow() == 1 ? 0 : i % b.nrow();
+
+    for (int j = 0; j < a.ncol(); ++j) {
+      upper[j] = a(a_row, j);
     }
 
-  else
-    { stop("No such parameter!");}
+    for (int j = 0; j < b.ncol(); ++j) {
+      lower[j] = b(b_row, j);
+    }
 
-  List out = List::create(_["u"] = outu,
-                          _["d"] = outd) ;
+    out(i, 0) = hypergeo_pfq(upper, lower, z[i % z.size()], max_terms);
+  }
+
   return out;
+}
 
+//' @describeIn splitt Gradients for the split-t CDF and log-density.
+//' @param y vector of quantiles for gradient evaluation.
+//' @param par list with \code{mu}, \code{df}, \code{phi}, and \code{lmd} parameter vectors.
+//' @param parCaller character scalar naming the parameter to differentiate:
+//' \code{"mu"}, \code{"df"}, \code{"phi"}, or \code{"lmd"}.
+//' @param denscaller character vector selecting gradients to compute. Use
+//' \code{"u"} for the CDF gradient and \code{"d"} for the log-density gradient.
+//' @export
+// [[Rcpp::export]]
+List gsplitt(NumericVector y, List par, std::string parCaller, GenericVector denscaller)
+{
+  const int n = y.size();
+  NumericVector mu = rep_len(as<NumericVector>(par["mu"]), n);
+  NumericVector df = rep_len(as<NumericVector>(par["df"]), n);
+  NumericVector phi = rep_len(as<NumericVector>(par["phi"]), n);
+  NumericVector lmd = rep_len(as<NumericVector>(par["lmd"]), n);
+
+  NumericVector outu(n, R_NaN);
+  NumericVector outd(n, R_NaN);
+
+  const std::string parameter = lower_string(parCaller);
+  const bool compute_u = denscaller.size() == 0 || starts_with_flag(denscaller, 'u');
+  const bool compute_d = denscaller.size() == 0 || starts_with_flag(denscaller, 'd');
+
+  if (parameter != "mu" && parameter != "df" &&
+      parameter != "phi" && parameter != "lmd") {
+    stop("No such parameter!");
+  }
+
+  for (int i = 0; i < n; ++i) {
+    const double nu = df[i];
+    const double scale = phi[i];
+    const double lambda = lmd[i];
+
+    if (invalid_parameters(nu, scale, lambda)) {
+      continue;
+    }
+
+    const double d = y[i] - mu[i];
+    const bool left = y[i] <= mu[i];
+    const double side = left ? 1.0 : lambda;
+    const double side2 = side * side;
+    const double one_plus_lambda = 1.0 + lambda;
+    const double beta0 = R::beta(0.5 * nu, 0.5);
+    const double denominator = d * d + side2 * nu * scale * scale;
+    const double a = side2 * nu * scale * scale / denominator;
+    const double a_power = std::pow(a, 0.5 * nu);
+    const double sqrt_denominator = std::sqrt(denominator);
+
+    if (compute_u) {
+      if (parameter == "mu") {
+        outu[i] = -2.0 * side * a_power /
+          (one_plus_lambda * beta0 * sqrt_denominator);
+      } else if (parameter == "df") {
+        const double multiplier = left ?
+          1.0 / one_plus_lambda :
+          -lambda / one_plus_lambda;
+        outu[i] = multiplier * regularized_beta_dnu(a, nu);
+      } else if (parameter == "phi") {
+        outu[i] = -2.0 * side * d * a_power /
+          (one_plus_lambda * scale * beta0 * sqrt_denominator);
+      } else if (parameter == "lmd") {
+        const double ibeta = ::Rf_pbeta(a, 0.5 * nu, 0.5, TRUE, FALSE);
+
+        if (left) {
+          outu[i] = -ibeta / (one_plus_lambda * one_plus_lambda);
+        } else {
+          outu[i] = -ibeta / (one_plus_lambda * one_plus_lambda) -
+            2.0 * d * a_power /
+              (one_plus_lambda * beta0 * sqrt_denominator);
+        }
+      }
+    }
+
+    if (compute_d) {
+      if (parameter == "mu") {
+        outd[i] = (1.0 + nu) * d / denominator;
+      } else if (parameter == "df") {
+        const double c0 = d * d - scale * scale * side2;
+        const double c3 = std::log(nu / (nu + d * d / (scale * scale * side2)));
+        const double digamma_diff = R::digamma(0.5 * nu) -
+          R::digamma(0.5 * (nu + 1.0));
+
+        outd[i] = 0.5 * (c0 / denominator + c3 - digamma_diff);
+      } else if (parameter == "phi") {
+        const double c0 = d * d - scale * scale * side2;
+        outd[i] = nu * c0 / (scale * denominator);
+      } else if (parameter == "lmd") {
+        if (left) {
+          outd[i] = -1.0 / one_plus_lambda;
+        } else {
+          outd[i] = -1.0 / one_plus_lambda +
+            (1.0 + nu) * d * d / (lambda * denominator);
+        }
+      }
+    }
+  }
+
+  return List::create(_["u"] = outu,
+                      _["d"] = outd);
 }
